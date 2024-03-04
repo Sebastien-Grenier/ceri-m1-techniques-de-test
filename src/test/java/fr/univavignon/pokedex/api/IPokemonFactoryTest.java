2050e1581083b43cd685d125659cd3584195b2e4
Sebastien-Grenier <sebastien.grenier@alumni.univ-avignon.fr>
2024-03-04 09:32:22 +0100

    test pokemonfactorytest

--- src/test/java/fr/univavignon/pokedex/api/IPokemonFactoryTest.java
+++ src/test/java/fr/univavignon/pokedex/api/IPokemonFactoryTest.java
@@ -1,4 +1,25 @@
 package fr.univavignon.pokedex.api;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
+
 public abstract class IPokemonFactoryTest {
+
+    @Mock
+    IPokemonFactory pokemonFactory;
+
+    @BeforeEach
+    public void setup() throws PokedexException {
+        when(pokemonFactory.createPokemon(0, 613, 64, 4000, 4)).thenReturn(new Pokemon(0, "Bulbizarre", 126, 126, 90, 613,64,4000,4, 56));
+    }
+
+    @Test
+    void shouldReturn64When0() throws PokedexException {
+        assertEquals(64, pokemonFactory.createPokemon(0, 613, -500, 4000, 4).getHp());
+    }
+
 }
