376397ada64f99040df42a784ef3f212d991d2ae
Sebastien-Grenier <sebastien.grenier@alumni.univ-avignon.fr>
2024-03-18 13:51:21 +0100

    ajout implémentation PokemonMetadataProvider

---
+++ src/main/java/fr/univavignon/pokedex/api/PokemonMetadataProvider.java
@@ -0,0 +1,31 @@
+package fr.univavignon.pokedex.api;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class PokemonMetadataProvider implements IPokemonMetadataProvider {
+
+    // Map pour stocker les métadonnées des Pokémon par index
+    private final Map<Integer, PokemonMetadata> metadataMap;
+
+    public PokemonMetadataProvider() {
+        // Initialisation de la map des métadonnées
+        this.metadataMap = new HashMap<>();
+        // Exemple d'ajout de métadonnées pour le Pokémon Bulbizarre (index 0)
+        this.metadataMap.put(0, new PokemonMetadata(0, "Bulbizarre", 126, 126, 90));
+        this.metadataMap.put(0, new PokemonMetadata(133, "Aquali", 186, 168, 260));
+        // Ajoutez d'autres métadonnées pour d'autres Pokémon si nécessaire
+    }
+
+    @Override
+    public PokemonMetadata getPokemonMetadata(int index) throws PokedexException {
+        // Vérifiez si les métadonnées pour cet index existent dans la map
+        if (metadataMap.containsKey(index)) {
+            // Si oui, retournez les métadonnées correspondantes
+            return metadataMap.get(index);
+        } else {
+            // Sinon, lancez une exception indiquant que l'index du Pokémon n'est pas valide
+            throw new PokedexException("Invalid Pokemon index: " + index);
+        }
+    }
+}
