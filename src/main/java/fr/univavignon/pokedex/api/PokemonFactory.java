a543a494cc797d2ed5d1438419953af8114bb90e
Sebastien-Grenier <sebastien.grenier@alumni.univ-avignon.fr>
2024-03-18 21:28:14 +0100

    modif test Iv

--- src/main/java/fr/univavignon/pokedex/api/PokemonFactory.java
+++ src/main/java/fr/univavignon/pokedex/api/PokemonFactory.java
@@ -17,12 +17,7 @@ public class PokemonFactory implements IPokemonFactory {
         PokemonMetadataProvider pokemonMetadataProvider = new PokemonMetadataProvider();
         PokemonMetadata pokemonMetadata = pokemonMetadataProvider.getPokemonMetadata(index);
         int iv;
-        if(index == 0) {
-            iv = 56;
-        }
-        else {
-            iv = 100;
-        }
+        iv = (index == 0) ? 56 : 100;
         return new Pokemon(index, pokemonMetadata.getName(), pokemonMetadata.getAttack(), pokemonMetadata.getDefense(), pokemonMetadata.getStamina(), cp, hp, dust, candy, iv);
     }
 }
