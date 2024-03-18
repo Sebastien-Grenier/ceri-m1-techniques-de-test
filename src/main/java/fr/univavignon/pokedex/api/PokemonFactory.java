29091ab36f622950e10c9440a0b9d98260fd9725
Sebastien-Grenier <sebastien.grenier@alumni.univ-avignon.fr>
2024-03-18 14:48:16 +0100

    modif impl PokemonMetadataProvider

--- src/main/java/fr/univavignon/pokedex/api/PokemonFactory.java
+++ src/main/java/fr/univavignon/pokedex/api/PokemonFactory.java
@@ -9,14 +9,11 @@ public class PokemonFactory implements IPokemonFactory {
     }
 
     @Override
-    public Pokemon createPokemon(int index, int cp, int hp, int dust, int candy) {
+    public Pokemon createPokemon(int index, int cp, int hp, int dust, int candy) throws PokedexException {
         // Récupérer les métadonnées du Pokémon à partir de l'index
         PokemonMetadata metadata = null;
-        try {
-            metadata = metadataProvider.getPokemonMetadata(index);
-        } catch (PokedexException e) {
-            throw new RuntimeException(e);
-        }
+        metadata = metadataProvider.getPokemonMetadata(index);
+
 
         // Créer une instance de Pokemon avec les métadonnées et les statistiques fournies
         return new Pokemon(index, metadata.getName(), metadata.getAttack(), metadata.getDefense(),
