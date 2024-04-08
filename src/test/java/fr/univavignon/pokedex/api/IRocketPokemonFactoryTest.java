bd90ba4d1ce48bb16e0b42f6f177fa969b2c3f15
Sebastien-Grenier <sebastien.grenier@alumni.univ-avignon.fr>
2024-04-08 19:08:07 +0200

    tests qui marchent avec changement du nom et index de bulbizarre

--- src/test/java/fr/univavignon/pokedex/api/IRocketPokemonFactoryTest.java
+++ src/test/java/fr/univavignon/pokedex/api/IRocketPokemonFactoryTest.java
@@ -10,19 +10,17 @@ public class IRocketPokemonFactoryTest {
 
     RocketPokemonFactory rocketPokemonFactory;
     Pokemon bulbizarre;
-    Pokemon aquali;
 
     @BeforeEach
     public void setup() throws PokedexException {
         rocketPokemonFactory = new RocketPokemonFactory();
-        bulbizarre = new Pokemon(0, "Bulbizarre", 126, 126, 90, 613, 64, 4000, 4, 56);
-        aquali = new Pokemon(133, "Aquali", 186, 168, 260, 2729, 202, 5000, 4, 100);
+        bulbizarre = new Pokemon(1, "Bulbasaur", 126, 126, 90, 613, 64, 4000, 4, 56);
     }
 
 
     @Test
     public void testCreatePokemonWithValidIndex() {
-        Pokemon createdPokemon = rocketPokemonFactory.createPokemon(0, 613, 64, 4000, 4);
+        Pokemon createdPokemon = rocketPokemonFactory.createPokemon(1, 613, 64, 4000, 4);
         assertNotNull(createdPokemon);
         assertEquals(createdPokemon.getName(), bulbizarre.getName());
         assertEquals(createdPokemon.getCp(), bulbizarre.getCp());
